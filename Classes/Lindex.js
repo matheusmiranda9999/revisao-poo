3f512b3a4b304b5e203c8dab8f1492dd1524f47d
matheusmiranda9999 <mirandamatheus482@gmail.com>
2025-08-26 22:28:27 -0300

    Create Lindex.js

---
+++ Classes/Lindex.js
@@ -0,0 +1,11 @@
+hconst Dragao = require('./Dragao');
+const Fada = require('./Fada');
+const Golem = require('./Golem');
+const Elemental = require('./Elemental');
+
+module.exports = {
+    Dragao,
+    Fada,
+    Golem,
+    Elemental
+};
